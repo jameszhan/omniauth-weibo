998f1c5386356d7ed34a30899c3ecc2c723286aa
James Zhan <zhiqiangzhan@gmail.com>
2013-03-06 16:22:29 +0800

    move access_token to credentials section.

--- lib/omniauth/strategies/weibo.rb
+++ lib/omniauth/strategies/weibo.rb
@@ -4,39 +4,42 @@ module OmniAuth
   module Strategies
     class Weibo < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site           => "https://api.weibo.com",
-        :authorize_url  => "/oauth2/authorize",
-        :token_url      => "/oauth2/access_token"
+        site:           "https://api.weibo.com",
+        authorize_url:  "value", "/oauth2/authorize",
+        token_url:      "/oauth2/access_token"
       }
       
       option :token_params, {
-        :parse          => :json
+        parse: :json
       }
       
       uid { raw_info['id'].to_s }
 
       info do
         {
-          :nickname     => raw_info['screen_name'],
-          :name         => raw_info['name'],
-          :location     => raw_info['location'],
-          :image        => raw_info['profile_image_url'],
-          :description  => raw_info['description'],
-          :urls => {
-            'Blog'      => raw_info['url'],
-            'Weibo'     => raw_info['domain'].present?? "http://weibo.com/#{raw_info['domain']}" : "http://weibo.com/u/#{raw_info['id']}",
+          nickname: raw_info['screen_name'],
+          name: raw_info['name'],
+          location: raw_info['location'],
+          image: raw_info['profile_image_url'],
+          description: raw_info['description'],
+          urls: {
+            'Blog' => raw_info['url'],
+            'Weibo' => raw_info['domain'].present?? "http://weibo.com/#{raw_info['domain']}" : "http://weibo.com/u/#{raw_info['id']}",
           }
         }
       end
 
+      credentials do
+        { access_token: access_token.token }
+      end
+  
       extra do
         { :raw_info => raw_info }
       end
 
       def raw_info
         uid = access_token[:uid] || access_token["uid"]
-        @token_info ||= access_token.params.merge("token" => access_token.token)
-        @raw_info ||= {"token_info" => @token_info}.merge(access_token.get("/2/users/show.json", :params => {:uid => uid}).parsed)
+        @raw_info ||= access_token.get("/2/users/show.json", params: {uid: uid}).parsed
       end
 
       def authorize_params
@@ -54,7 +57,7 @@ module OmniAuth
       
       protected
         def build_access_token
-          super.tap{ |access_token| access_token.options.merge!(:mode => :query, :param_name => :access_token) }
+          super.tap{ |access_token| access_token.options.merge!(mode: :query, param_name: :access_token) }
         end
       
     end
